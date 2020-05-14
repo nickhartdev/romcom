67ef0e27de1825b4ef0774c756f708885b9684d8
Derek Romero <dereklromero13@gmail.com>
2020-05-14 17:42:42 -0600

    Refactor iter-0 code

--- src/main.js
+++ src/main.js
@@ -44,12 +44,7 @@ function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
 
-var randomCover = covers[getRandomIndex(covers)];
-var randomTitle = titles[getRandomIndex(titles)];
-var randomTagline1 = descriptors[getRandomIndex(descriptors)];
-var randomTagline2 = descriptors[getRandomIndex(descriptors)];
-
-coverImage.src = randomCover;
-coverTitle.innerText = randomTitle;
-tagline1.innerText = randomTagline1;
-tagline2.innerText = randomTagline2;
+coverImage.src = covers[getRandomIndex(covers)];
+coverTitle.innerText = titles[getRandomIndex(titles)];
+tagline1.innerText = descriptors[getRandomIndex(descriptors)];
+tagline2.innerText = descriptors[getRandomIndex(descriptors)];
