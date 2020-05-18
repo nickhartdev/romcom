b59862a056b890e7765ee55867d77bf6942905d3
Nick Hart <nick.hart.des@gmail.com>
2020-05-17 22:16:22 -0600

    Add saveCover function - adds current cover to savedCovers array, and inserts saved cover into gallery

--- src/main.js
+++ src/main.js
@@ -18,14 +18,13 @@ var createNewBookBtn = document.querySelector(".create-new-book-button");
 var homeViewPage = document.querySelector(".home-view");
 var formViewPage = document.querySelector(".form-view");
 var savedViewPage = document.querySelector(".saved-view");
+var savedCoversSection = document.querySelector(".saved-covers-section")
 
 //Form input fields
 var coverImageInput = document.querySelector("#cover");
 var titleInput = document.querySelector("#title");
 var descriptor1Input = document.querySelector("#descriptor1");
 var descriptor2Input = document.querySelector("#descriptor2");
-
-//global variables
 
 // We've provided a few variables below
 var savedCovers = [
@@ -39,9 +38,7 @@ makeOwnCoverBtn.addEventListener("click", showFormPage);
 viewSavedBtn.addEventListener("click", showSavedPage);
 homeBtn.addEventListener("click", showHomePage);
 createNewBookBtn.addEventListener("click", saveCreatedCover);
-
-
-// Create your event handlers and other functions here 👇
+saveCoverBtn.addEventListener("click", saveCover);
 
 // Randomized Book Cover Functions
 function showRandomCover() {
@@ -79,6 +76,7 @@ function showSavedPage() {
   saveCoverBtn.classList.add("hidden");
   savedViewPage.classList.remove("hidden");
   homeBtn.classList.remove("hidden");
+  showSavedCovers();
 }
 
 function showHomePage() {
@@ -90,38 +88,43 @@ function showHomePage() {
   randomCoverBtn.classList.remove("hidden");
 }
 
-function saveCreatedCover(event) {
-  event.preventDefault();
-  var coverUrl = coverImageInput.value;
-  var title = titleInput.value;
-  var descriptor1 = descriptor1Input.value;
-  var descriptor2 = descriptor2Input.value;
-  covers.push(coverUrl);
-  titles.push(title);
-  descriptors.push(descriptor1, descriptor2);
-  currentCover = new Cover(coverUrl, title, descriptor1, descriptor2);
+function saveCreatedCover() {
+  currentCover = new Cover(coverImageInput.value, titleInput.value, descriptor1Input.value, descriptor2Input.value);
+  covers.push(coverImageInput.value);
+  titles.push(titleInput.value);
+  descriptors.push(descriptor1Input.value, descriptor2Input.value);
   displayCreatedCover();
+  event.preventDefault();
 }
 
 function displayCreatedCover() {
-  var coverUrl = coverImageInput.value;
-  var title = titleInput.value;
-  var descriptor1 = descriptor1Input.value;
-  var descriptor2 = descriptor2Input.value;
-  coverImage.src = coverUrl;
-  coverTitle.innerText = title;
-  tagline1.innerText = descriptor1;
-  tagline2.innerText = descriptor2;
-  showHomePage()
+  coverImage.src = coverImageInput.value;
+  coverTitle.innerText = titleInput.value;
+  tagline1.innerText = descriptor1Input.value;
+  tagline2.innerText = descriptor2Input.value;
+  showHomePage();
 }
 
+function saveCover() {
+  currentCover = new Cover(coverImage.src, coverTitle.innerHTML, tagline1.innerHTML, tagline2.innerHTML);
+  savedCovers.push(currentCover);
+  savedCoversSection.insertAdjacentHTML('afterbegin', `<section class='mini-cover'>
+  <img class="cover-image" src=${currentCover.cover}><h2 class="cover-title">${currentCover.title}</h2>
+  <h3 class="tagline">A tale of <span class="tagline-1">${currentCover.tagline1}</span> and <span class="tagline-2">${currentCover.tagline2}</span></h3>
+  <img class="price-tag" src="./assets/price.png">
+  <img class="overlay" src="./assets/overlay.png"></section>`)
+}
+//
+// function showSavedCovers() {
+//   for (i = 0; i < savedCovers.length; i++) {
+//   }
+// }
 
-
-// We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
 
+// Shows random cover on page reload
 coverImage.src = covers[getRandomIndex(covers)];
 coverTitle.innerText = titles[getRandomIndex(titles)];
 tagline1.innerText = descriptors[getRandomIndex(descriptors)];
